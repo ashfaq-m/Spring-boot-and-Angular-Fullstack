295f62b295627f0aaa207f837e481689519802fe
ashfaq-m <ashapak.makandar7@gmail.com>
2024-06-30 15:31:05 +0530

    Simple form created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/my-first-component/my-first-component.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-component',
+  templateUrl: './my-first-component.component.html',
+  styleUrls: ['./my-first-component.component.scss']
+})
+export class MyFirstComponentComponent {
+
+  name : string = '';
+  email : string = '';
+  message : string = '';
+  isSubmitted : boolean = false;
+  
+}
